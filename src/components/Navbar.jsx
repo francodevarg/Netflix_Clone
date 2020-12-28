0353937775dc7b169b7f829e664a96756da00817
Franco Narvaez <jnarvaez@est.frba.utn.edu.ar>
2020-12-27 22:44:51 -0300

    Agrego interación a la busqueda Responsive
    Se añade interacción al clickear en la cruz
    para limpiar el formulario de busqueda.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import '../assets/styles/components/Navbar.scss';
 import logo from '../assets/static/netflix-logo.png';
+import imgNotFound from '../assets/static/no-disponible.png';
 import requests from '../requests';
 import axios from '../axios';
 
@@ -39,16 +40,34 @@ const Navbar = () => {
   const [query, setQuery] = useState('');
   const [results, setSearchResults] = useState([]);
 
-  function handleChange(e) {
-    const input = document.getElementById('input');
-    setQuery(input.value);
-    if (input.value !== '') {
-      axios.get(`${requests.search}&query=${query}`).then((response) => {
-        console.log(response.data.results);
-        setSearchResults(response.data.results);
-      });
+  function handleSearchQuery(e) {
+    setQuery(e.target.value);
+    if (e.target.value.length > 2) {
+      axios
+        .get(`${requests.search}&query=${query}`)
+        .then((response) => {
+          setSearchResults(response.data.results);
+        })
+        .catch((error) => console.log(error));
     }
   }
+
+  const [crossSearch, setCrossSearch] = useState(false);
+  const activeCrossSearch = crossSearch ? 'yes-display' : 'no-display';
+
+  const handleCrossRender = (e) => {
+    if (e.target.value.length > 0) {
+      setCrossSearch(true);
+    } else if (e.target.value.length === 0) {
+      setCrossSearch(false);
+    }
+  };
+
+  const cleanSearch = (e) => {
+    setQuery('');
+    e.target.name = '';
+    setCrossSearch(false);
+  };
 
   return (
     <>
@@ -110,32 +129,45 @@ const Navbar = () => {
             <input
               className='search__input'
               type='text'
-              id='input'
+              name='searchInput'
               placeholder='Buscar en Netflix'
-              onInput={handleChange}
+              autoComplete='off'
+              onInput={handleSearchQuery}
+              onChange={handleCrossRender}
+              value={query}
             />
             <div className='search__icon'>
               <box-icon name='search' size='sm' color='#fff' />
             </div>
 
-            <div className='search__cross'>
-              <box-icon name='x-circle' type='solid' color='#e50914' />
+            <div className={`search__cross ${activeCrossSearch}`}>
+              <button type='button' onClick={cleanSearch}>
+                <box-icon name='x-circle' type='solid' color='#e50914' />
+              </button>
             </div>
           </div>
         </div>
         <div className='search__results'>
           <ul className='search__results-list'>
             {!query && <li>Ingrese el nombre de una película o serie</li>}
-            {results.map((movie) => (
-              <li className='search__results-element '>
-                <img
-                  className='results-img'
-                  src={`${IMAGE_BASE_URL}${movie.poster_path || movie.profile_path || movie.profile_path}`}
-                  alt={movie.name}
-                />
-                <h6 className='results-title'>{movie.title || movie.name}</h6>
-              </li>
-            ))}
+            {query.length > 0 &&
+              results.map((movie) => (
+                <li className='search__results-element ' key={movie.id}>
+                  {(movie.media_type === 'tv' || movie.media_type === 'movie') && movie.poster_path && (
+                    <img className='results-img' src={`${IMAGE_BASE_URL}${movie.poster_path}`} alt={movie.name} />
+                  )}
+                  {(movie.media_type === 'tv' || movie.media_type === 'movie') && !movie.poster_path && (
+                    <img className='results-img' src={imgNotFound} alt={movie.name} />
+                  )}
+                  {movie.media_type === 'person' && movie.profile_path && (
+                    <img className='results-img' src={`${IMAGE_BASE_URL}${movie.profile_path}`} alt={movie.name} />
+                  )}
+                  {movie.media_type === 'person' && !movie.profile_path && (
+                    <img className='results-img' src={imgNotFound} alt={movie.name} />
+                  )}
+                  <h6 className='results-title'>{movie.title || movie.name}</h6>
+                </li>
+              ))}
           </ul>
         </div>
       </div>
