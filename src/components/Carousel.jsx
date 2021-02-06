d30f59da880f4948ca9e474f6c588661dcaae6dd
Franco Narvaez <jnarvaez@est.frba.utn.edu.ar>
2021-02-06 18:24:18 -0300

    Se añade la lógica de las flechas del Carousel.
    Ahora se puede hacer Scroll con las flechas.
    Además al principio (sin Scroll) la flecha izquierda
    no se muestra, por cuestiones de diseño CSS.

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,18 +1,40 @@
-import React from 'react';
+import React , {useRef,useState} from 'react';
 import '../assets/styles/components/Carousel.scss';
 
 const Carousel = ({ children, isLargeRow }) => {
+  let carouselContainer = useRef(null);
+  const [stateArrowLeft, setstateArrowLeft] = useState(false);
+
+  let lfArrowDisplay =  stateArrowLeft ? ' ' : 'no-display';
+
   const largeBtn = isLargeRow ? 'carousel__btn carousel__btn--large' : 'carousel__btn';
+ 
+  const flechaIzquierda = () => {
+    carouselContainer.current.scrollLeft -= (carouselContainer.current.offsetWidth / 5);
+    if(carouselContainer.current.scrollLeft === 0){
+      setstateArrowLeft(false);
+    }
+  }
+  
+  const flechaDerecha = () => {
+    carouselContainer.current.scrollLeft += (carouselContainer.current.offsetWidth / 5);
+    console.log(carouselContainer.current.scrollLeft);
+    if(carouselContainer.current.scrollLeft !== 0){
+      setstateArrowLeft(true);
+    }
+  }
+
+
 
   return (
-    <div className='carousel'>
-      <button className={`${largeBtn} carousel__btn--left`} type='button'>
+    <div ref={carouselContainer} className='carousel'>
+      <button className={`${largeBtn} carousel__btn--left ${lfArrowDisplay}`} onClick={flechaIzquierda} type='button'>
         <box-icon type='solid' name='chevron-left' color='#fff' />
       </button>
-      <button className={`${largeBtn} carousel__btn--right`} type='button'>
+      <button className={`${largeBtn} carousel__btn--right`} onClick={flechaDerecha} type='button'>
         <box-icon type='solid' name='chevron-right' color='#fff' />
       </button>
-      <div className='carousel__container'>{children}</div>
+      <div  className='carousel__container'>{children}</div>
     </div>
   );
 };
