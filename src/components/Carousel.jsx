570a2d358a39e9a468e1f6fd463c248a9d8ce220
Franco Narvaez <jnarvaez@est.frba.utn.edu.ar>
2021-02-08 23:54:30 -0300

    Se modifican las flechas del Carousel
    Se agrandan los iconos y se agrega lógica de
    flecha derecha cuando llega al final desaparece.

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -4,8 +4,10 @@ import '../assets/styles/components/Carousel.scss';
 const Carousel = ({ children, isLargeRow }) => {
   let carouselContainer = useRef(null);
   const [stateArrowLeft, setstateArrowLeft] = useState(false);
+  const [stateArrowRight, setstateArrowRight] = useState(true);
 
   let lfArrowDisplay =  stateArrowLeft ? ' ' : 'no-display';
+  let rightArrowDisplay =  stateArrowRight ? ' ' : 'no-display';
 
   const largeBtn = isLargeRow ? 'carousel__btn carousel__btn--large' : 'carousel__btn';
  
@@ -14,13 +16,20 @@ const Carousel = ({ children, isLargeRow }) => {
     if(carouselContainer.current.scrollLeft === 0){
       setstateArrowLeft(false);
     }
+    if((carouselContainer.current.scrollWidth - carouselContainer.current.scrollLeft) !== carouselContainer.current.clientWidth ){
+      setstateArrowRight(true);
+    }
   }
   
   const flechaDerecha = () => {
     carouselContainer.current.scrollLeft += (carouselContainer.current.offsetWidth / 5);
     console.log(carouselContainer.current.scrollLeft);
+    console.log(carouselContainer);
     if(carouselContainer.current.scrollLeft !== 0){
       setstateArrowLeft(true);
+    }
+    if((carouselContainer.current.scrollWidth - carouselContainer.current.scrollLeft) === carouselContainer.current.clientWidth ){
+      setstateArrowRight(false);
     }
   }
 
@@ -29,10 +38,10 @@ const Carousel = ({ children, isLargeRow }) => {
   return (
     <div ref={carouselContainer} className='carousel'>
       <button className={`${largeBtn} carousel__btn--left ${lfArrowDisplay}`} onClick={flechaIzquierda} type='button'>
-        <box-icon type='solid' name='chevron-left' color='#fff' />
+        <box-icon type='solid' name='chevron-left' color='#fff' size='md'/>
       </button>
-      <button className={`${largeBtn} carousel__btn--right`} onClick={flechaDerecha} type='button'>
-        <box-icon type='solid' name='chevron-right' color='#fff' />
+      <button className={`${largeBtn} carousel__btn--right ${rightArrowDisplay}`} onClick={flechaDerecha} type='button'>
+        <box-icon type='solid' name='chevron-right' color='#fff' size='md'/>
       </button>
       <div  className='carousel__container'>{children}</div>
     </div>
