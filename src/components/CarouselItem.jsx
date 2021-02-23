d3eb8984a9d34f4c90f4290797daaf624a918110
Franco Narvaez <jnarvaez@est.frba.utn.edu.ar>
2021-02-22 23:43:58 -0300

    Cambios en los botones del Carousel.
    Se sacan los botones de like, y se agrega el de play en
    CarouselItem, con una lógica cuando agrega a la lista
    cambia el icono de plus -> a check.
    Además se cambia el color de las flechas del Carousel
    dandoles más transparencias.

--- src/components/CarouselItem.jsx
+++ src/components/CarouselItem.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import '../assets/styles/components/CarouselItem.scss';
 
 const IMAGE_BASE_URL = 'https://image.tmdb.org/t/p/original';
@@ -6,6 +6,15 @@ const IMAGE_BASE_URL = 'https://image.tmdb.org/t/p/original';
 const CarouselItem = ({ name, title, posterPath, backdropPath, voteAverage, isLargeRow }) => {
   const largeItem = isLargeRow ? 'carousel-item--large' : '';
   const largeImg = isLargeRow ? 'carousel-item__img--large' : '';
+
+  const [itemToList , setItemToList] = useState(false);
+
+  const listBtn = itemToList ? 'check' : 'plus';
+  
+  const addList =  () => {
+    setItemToList( ! itemToList);
+  }
+  
 
   return (
     <div className={`carousel-item ${largeItem}`}>
@@ -16,22 +25,15 @@ const CarouselItem = ({ name, title, posterPath, backdropPath, voteAverage, isLa
       />
       <div className='carousel-item__detail'>
         <div className='detail-action'>
-          <a className='action' href='#t'>
-            <box-icon name='chevron-down' type='solid' color='#a0a0a0' border='circle' size='md'/>
+          <a className='action action--play' href='#t'>
+            <box-icon name='play'  color='#000' border='circle' size='md'/>
           </a>
           <a className='action' href='#te'>
-            <box-icon name='plus' color='#a0a0a0' border='circle' size='md' />
+            <box-icon name={`${listBtn}`} onClick={addList} color='#a0a0a0' border='circle' size='md' />
           </a>
-          { isLargeRow && 
-                    <>
-                    <a className='action' href='#te'>
-                      <box-icon name='like' color='#a0a0a0' border='circle' size='md' />
-                    </a>
-                    <a className='action' href='#te'>
-                        <box-icon name='dislike' color='#a0a0a0' border='circle' size='md' />
-                    </a>
-                    </>
-          }
+          <a className='action' href='#te'>
+            <box-icon name='chevron-down' type='solid' color='#a0a0a0' border='circle' size='md' />
+          </a>
         </div>
         <p className='detail__title'>{name || title}</p>
         <p className='detail__subtitle'>
